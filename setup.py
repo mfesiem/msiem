28bd8b1c5ce309186c7eabba4469656345b9fdee
Tristan Landes-Tremblay <tristan.landes-tremblay@radio-canada.ca>
2019-05-03 09:23:28 -0400

    better setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,15 @@
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
+from setuptools import setup
+import pathlib
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
 
 setup(
     name='msiem',
+    description="McAfee SIEM Command Line Interface and Python API",
     url='https://github.com/tristanlatr/msiem',
     version='0.0.1',
     packages=['msiem',],
@@ -14,7 +19,11 @@ setup(
     install_requires=[
           'requests',
     ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+    ],
     license='The MIT License',
-    long_description=open('README.md').read(),
+    long_description=README,
+    long_description_content_type="text/markdown",
     test_suite="tests"
 )
